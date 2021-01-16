cafabbbb861ef5d476f9e5437747cd966d36e272
Sarah Eisa <prosarah10@gmail.com>
2021-01-17 01:59:16 +0200

    Feat: Create cart actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,6 @@
 export const ADD_FETCHED_DATA = 'ADD_FETCHED_DATA';
 export const ADD_FETCHED_CATEGORY = 'ADD_FETCHED_CATEGORY';
 export const SEARCH_OPTIONS = 'SEARCH_OPTIONS';
+export const FETCH_CART = 'FETCH_CART';
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
