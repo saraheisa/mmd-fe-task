4f282c7a395d0e65e8427dbec5ec070daf79a07a
Sarah Eisa <prosarah10@gmail.com>
2021-01-16 23:03:16 +0200

    Feat: create categories reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import productsReducer from './productsReducer';
+import categoriesReducer from './categoriesReducer';
 
 export default combineReducers({
-    products: productsReducer
+    products: productsReducer,
+    categories: categoriesReducer
 });
