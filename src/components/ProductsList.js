0864f2468c590dc73f5b522dfcdb704cd856df3b
Sarah Eisa <prosarah10@gmail.com>
2021-01-16 15:19:44 +0200

    Style: style products list

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import '../css/ProductsList.css';
 
 const ProductsList = () => {
     const state = useSelector(state => state);
 
-    return(
+    return (
         <ul className="products-list">
-            {state.products.map( product => 
-                <li className="product">
-                    <div>
+            {state.products.map(product =>
+                <li className="product" key={product.id}>
+                    <div className="product-img">
                         <img src={product.image} alt={product.name} />
                     </div>
-                    <div>
+                    <div className="product-category">
                         {product.category}
                     </div>
-                    <div>
+                    <div className="product-details">
                         <h2>{product.title}</h2>
                         <div>${product.price}</div>
-                        <div>
-                            <button>add to cart</button>
-                        </div>
+                    </div>
+                    <div className="product-add-btn">
+                        <button>add to cart</button>
                     </div>
                 </li>
             )}
