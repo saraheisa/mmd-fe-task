d577f984b7de23a0badf059b9553cd5c57b22c35
Sarah Eisa <prosarah10@gmail.com>
2021-01-17 02:00:43 +0200

    Feat: Set cart from local storage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ProductsList from './components/ProductsList';
 import Search from './components/Search';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Search />
+        <Cart />
         <Switch>
           <Route
             exact
